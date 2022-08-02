5c6ae5c179cd7c7ec6e17ba88be8e7316ed290eb
Harshit <harshit.nyati>
2022-08-02 22:52:10 +0530

    lsm cdk

--- deployment/assets/model_v1/build.py
+++ deployment/assets/model_v1/build.py
@@ -1,6 +1,7 @@
 # download sentence transformer model and create model.tar.gz package file
 import os
 import tarfile
+import zipfile
 from sentence_transformers import models, SentenceTransformer
 
 
@@ -14,6 +15,18 @@ if not os.path.isdir(STAGING_DIR):
 model = SentenceTransformer("distilbert-base-nli-stsb-mean-tokens")
 model.save(STAGING_DIR)
 
-with tarfile.open(os.path.join(OUTPUT_DIR, "model.tar.gz"), mode="w:gz") as archive:
-    archive.add(STAGING_DIR, recursive=True)
-    # archive.add(CODE_DIR, recursive=True)
+# with tarfile.open(os.path.join(OUTPUT_DIR, "model.tar.gz"), mode="w:gz") as archive:
+#     archive.add(STAGING_DIR, recursive=True)
+#     # archive.add(CODE_DIR, recursive=True)
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(
+                os.path.join(root, file),
+                os.path.relpath(os.path.join(root, file), os.path.join(path)),
+            )
+
+
+with zipfile.ZipFile(OUTPUT_DIR, "w", zipfile.ZIP_DEFLATED) as zipf:
+    zipdir(STAGING_DIR, zipf)
--- deployment/test.py
+++ deployment/test.py
@@ -1,5 +1,30 @@
 from os import path
 import tarfile
+import os
+import zipfile
 
 print(path.join("deployment", "assets", "model_v1"))
 print(tarfile)
+
+OUTPUT_DIR = "/Users/nyatih/github/lsm/deployment/assets/"
+STAGING_DIR = "/Users/nyatih/github/lsm/deployment/assets/model_v1/code/"
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(
+                os.path.join(root, file),
+                os.path.relpath(os.path.join(root, file), os.path.join(path)),
+            )
+
+
+with zipfile.ZipFile(
+    "/Users/nyatih/github/lsm/deployment/assets/model.zip", "w", zipfile.ZIP_DEFLATED
+) as zipf:
+    zipdir("/Users/nyatih/github/lsm/deployment/assets/model_v1/code/", zipf)
+
+# with tarfile.open(os.path.join(OUTPUT_DIR, "model.tar.gz"), mode="w:gz") as archive:
+#     archive.add(STAGING_DIR, recursive=True)
+
